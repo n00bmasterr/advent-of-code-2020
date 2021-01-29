d2eb708fad9eda1198c2584ad5bc2161215708f4
n00bmasterr <55821961+n00bmasterr@users.noreply.github.com>
2021-01-29 12:04:21 +1100

    Day2 part2 rust

--- rust/day2/src/part2.rs
+++ rust/day2/src/part2.rs
@@ -7,12 +7,15 @@ pub fn run() {
     for lines in split_newline {
         let split_spaces: Vec<&str> = lines.split(" ").collect();
         let split_nums: Vec<&str> = split_spaces[0].split("-").collect();
-        let first_index = split_nums[1].parse().unwrap() - 1;
-        let second_index = split_nums[0].parse().unwrap() - 1;
-        let mut character = split_spaces[1].to_string().chars();
+        let first_index: u16 = split_nums[1].parse::<u16>().unwrap() - 1;
+        let second_index: u16 = split_nums[0].parse::<u16>().unwrap() - 1;
+        let mut character = split_spaces[1].to_string();
         character.pop(); // Remove the last :
-        if Some(character) == split_spaces[2].chars().nth(0) {
-            println!("{}",character);
+        let test = character.chars().nth(0).unwrap();
+        if split_spaces[2].chars().nth(first_index as usize) == Some(test) && split_spaces[2].chars().nth(second_index as usize) != Some(test) {
+             final_count += 1;
+        } else if split_spaces[2].chars().nth(first_index as usize) != Some(test) && split_spaces[2].chars().nth(second_index as usize) == Some(test) {
+            final_count += 1;
         }
     }
     println!("The answer is: {}", final_count);
